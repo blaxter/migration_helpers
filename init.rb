24d065a8a3495c62cd202ec0ff52561b3f414ec8
Jesús García Sáez <blaxter@gmail.com>
2009-04-03 20:20:45 +0200

    better way of include in adapters

--- init.rb
+++ init.rb
@@ -1,9 +1,4 @@
 require 'migration_helper'
 
-ActiveRecord::ConnectionAdapters.constants.grep( /Adapter$/ ).
-each do |adapter|
-  if ActiveRecord::ConnectionAdapters.const_defined? adapter
-    ActiveRecord::ConnectionAdapters.const_get( adapter ).
-      send :include, MigrationConstraintHelpers
-  end
-end
+ActiveRecord::ConnectionAdapters::AbstractAdapter.
+  send :include, MigrationConstraintHelpers
