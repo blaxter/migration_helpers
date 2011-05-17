2836fd9f88e1df3e4ed69b38d4318c71c041c0f0
Rich Meyers <richmeyers@gmx.com>
2011-05-17 10:03:06 -0400

    Renamed migration_helper.rb to migration_helpers.rb to be consistent with package name

--- init.rb
+++ init.rb
@@ -2,6 +2,6 @@ LIB_PATH = File.join(
     File.expand_path( File.dirname __FILE__ ), 'lib'
   )
 
-require File.join( LIB_PATH, 'migration_helper' )
+require File.join( LIB_PATH, 'migration_helpers' )
 
 ActiveRecord::Migration.extend MigrationConstraintHelpers
